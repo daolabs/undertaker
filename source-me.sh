d6779927054965c3b7dac1c038148b2ff4b5d21a
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-09-05 01:43:38 +0200

    remove due to use of docker image sys42/docker-go-builder

--- source-me.sh
+++
@@ -1,8 +0,0 @@
-export GOPATH="$(pwd)/external/":"$(pwd)/"
-export GOBIN="$(pwd)/build"
-echo "$PATH" | grep "$GOBIN" 1>&2 2>/dev/null
-[ $? -ne 0 ] && export PATH="$GOBIN":"$PATH"
-
-echo "GOPATH = $GOPATH"
-echo "GOBIN  = $GOBIN"
-echo "PATH   = $PATH"
